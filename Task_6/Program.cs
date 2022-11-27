56c00434561d888b47c9d1ac2165d12ec1163866
unknown <xarus82@mail.ru>
2022-11-27 16:52:29 +0300

    Решение задачи 2

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = number % 2;
+
+if (evenNumber != 0) Console.WriteLine($"Число {number} нечетное");
+
+else Console.WriteLine($"Число {number} четное");
